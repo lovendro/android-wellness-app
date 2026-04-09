8a659585360dffb7ee72ebb41deb4085e3de9fd6
lovendro <lovendro.p@gmail.com>
2026-04-09 10:15:21 +0300

    added ads

--- app/src/main/java/com/lovendro/farasi_wellnessapp/MainActivity.kt
+++ app/src/main/java/com/lovendro/farasi_wellnessapp/MainActivity.kt
@@ -1,18 +1,54 @@
 package com.lovendro.farasi_wellnessapp
 
 import android.content.Intent
+//import android.media.tv.AdRequest
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.LoadAdError
+
+
+import com.google.android.gms.ads.interstitial.InterstitialAd
+import com.google.android.gms.ads.interstitial.InterstitialAdLoadCallback
+
 
 class MainActivity : AppCompatActivity() {
+
+//    declare a variable to store our interstitial ad
+    private var mInterstitialAd: InterstitialAd?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+//        implementation of the banner add
+//        starts the google admob sdk
+//        prepares your app to load ads
+//        must be called before showing ads
+        MobileAds.initialize(this)
+
+//        get the ad view from layout
+//        connects your kotlin code to the adview xml
+        val adView=findViewById<AdView>(R.id.adView)
+
+//        creates request asking admob for an ad
+        val adRequest= AdRequest.Builder().build()
+
+//        load the ad
+//        send request admob
+//        admob returns an ad
+//        the add is displayed in your app
+        adView.loadAd(adRequest)
+
+        loadInterstitialAd()
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -29,6 +65,8 @@ class MainActivity : AppCompatActivity() {
 //            set your intent
             val recipeIntent= Intent(applicationContext, HealthyRecipes::class.java)
             startActivity(recipeIntent)
+
+            showInterstitialAd()
 
 
         }
@@ -84,7 +122,34 @@ class MainActivity : AppCompatActivity() {
         }
 
 
+
+
+    }
+//a function to load our ad from the server
+fun loadInterstitialAd() {
+    val adRequest = AdRequest.Builder().build()
+
+    InterstitialAd.load(
+        this,
+        "ca-app-pub-3940256099942544/1033173712", // Test ID
+        adRequest,
+        object : InterstitialAdLoadCallback() {
+
+            override fun onAdLoaded(ad: InterstitialAd) {
+                mInterstitialAd = ad
+            }
+
+            override fun onAdFailedToLoad(error: LoadAdError) {
+                mInterstitialAd = null
+            }
+        }
+    )
+}
+    //Show Interstitial ad
+    fun showInterstitialAd() {
+        if (mInterstitialAd != null) {
+            mInterstitialAd?.show(this)
+        }
     }
 
-
-}
+    }
